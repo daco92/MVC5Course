4a7b3a697300cff5d70315c2886c904d699ae9ee
Daco <daco.chen@gmail.com>
2016-10-15 16:40:58 +0800

    16 建立 ClientLoginViewModel 並製作客戶登入頁面與登入結果頁面

---
+++ MVC5Course/Models/ViewModels/ClientLoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models.ViewModels
+{
+    public class ClientLoginViewModel
+    {
+        [Required]
+        [DisplayName("名")]
+        [StringLength(10, ErrorMessage = "{0}最大不可超過{1}個字元")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(10)]
+        [DisplayName("中間名")]
+        public string MiddleName { get; set; }
+        [Required]
+        [StringLength(10)]
+        [DisplayName("姓")]
+        public string LastName { get; set; }
+       
+        [DisplayName("生日")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> DateOfBirth { get; set; }
+    }
+}
