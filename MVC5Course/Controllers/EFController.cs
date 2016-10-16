a9b8df2a440e12c78be179dd025298380f0c4828
Daco <daco.chen@gmail.com>
2016-10-16 16:32:09 +0800

    25 練習對 Entity Framework 的資料存取程式進行效能調校

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -1,4 +1,5 @@
 ﻿using MVC5Course.Models;
+using MVC5Course.Models.ViewModels;
 using System.Data.Entity.Validation;
 using System.Linq;
 using System.Web.Mvc;
@@ -13,7 +14,7 @@ namespace MVC5Course.Controllers
         public ActionResult Index()
         {
             var db = new FabricsEntities();
-            var data = db.Product.Where(c => c.ProductName.Contains("White"));
+            var data = db.Product.Where(c => c.ProductName.Contains("White")).Take(50);
 
             return View(data);
         }
@@ -73,24 +74,52 @@ namespace MVC5Course.Controllers
             return RedirectToAction("Index");
         }
 
+        //public ActionResult Add20Percent()
+        //{
+        //    var data = db.Product.Where(c => c.ProductName.Contains("White"));
+        //    foreach (var item in data)
+        //    {
+        //        if (item.Price.HasValue)
+        //        {
+        //            item.Price = item.Price * 1.2m;
+        //        }
+        //    }
+        //    db.SaveChanges();
+
+        //    return RedirectToAction("Index");
+        //}
+
         public ActionResult Add20Percent()
         {
-            var data = db.Product.Where(c => c.ProductName.Contains("White"));
-            foreach (var item in data)
-            {
-                if (item.Price.HasValue)
-                {
-                    item.Price = item.Price * 1.2m;
-                }
-            }
-            db.SaveChanges();
-
+            string key = "White";
+            db.Database.ExecuteSqlCommand(
+                @"Update dbo.Product set Price=Price*1.2 where ProductName like @p0 ", "%" + key + "%");
             return RedirectToAction("Index");
         }
 
         public ActionResult ViewLoad()
         {
             var data = db.vw_ClientContribution.Take(10);
+            return View(data);
+        }
+
+        public ActionResult ViewLoad2(string keyword = "Mary")
+        {
+            var data = db.Database.SqlQuery<ClientViewLoadViewModel>(
+                  @"SELECT
+		                 c.ClientId,
+		                 c.FirstName,
+		                 c.LastName,
+		                 (SELECT SUM(o.OrderTotal)
+		                  FROM [dbo].[Order] o
+		                  WHERE o.ClientId = c.ClientId) as OrderTotal
+	                FROM
+		                [dbo].[Client] as c
+
+                    Where c.FirstName like @p0
+                    ", "%" + keyword + "%"
+                  );
+
             return View(data);
         }
     }
